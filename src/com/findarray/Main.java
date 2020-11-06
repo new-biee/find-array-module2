cd49f7b82e10fc30885ba76e8582771558b0c1c5
new-biee <anhvtth1907004@fpt.edu.vn>
2020-11-06 12:00:06 +0700

    Add all file

---
+++ src/com/findarray/Main.java
@@ -0,0 +1,26 @@
+package com.findarray;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a name's student: ");
+        String input_name = scanner.nextLine();
+
+        boolean isExit = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(input_name)) {
+                System.out.println("Postion of the students in the list " + input_name + " is: " + (i + 1));
+                isExit = true;
+                break;
+            }
+        }
+        if (!isExit) {
+            System.out.println("Not found " + input_name + " in the list.");
+        }
+    }
+}
